44ba4f57ed1c14f93911fe29d2b7f5ec805e970d
insaatmuhendisi <hiandic@gmail.com>
2022-03-09 17:00:59 +0300

    Initial commit

---
+++ app/src/main/java/com/andic/catchthekenny2/Home.java
@@ -0,0 +1,37 @@
+package com.andic.catchthekenny2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Home extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+        textView = findViewById(R.id.textView2);
+    }
+
+    public void start(View view){
+        Intent intent = new Intent(Home.this,MainActivity.class);
+        startActivity(intent);
+    }
+
+}
